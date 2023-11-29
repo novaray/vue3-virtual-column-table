1a574574a35508bfa0919a3920a478b84ddafa45
skkim <novaray@naver.com>
2023-11-29 22:01:03 +0900

    - 예제 헤더 생성 함수 추가. - 데이터 생성 쪽 데이터 추가.

--- src/components/virtualColumnPaginationTable/VirtualColumnTableExampleData.ts
+++ src/components/virtualColumnPaginationTable/VirtualColumnTableExampleData.ts
@@ -1,15 +1,18 @@
 export const getVirtualColumnTableExampleData = (arraySize: number, columnSize: number) => {
   const data = [];
   
-  for (let i = 0; i < arraySize; i++) {
+  for (let i = 1; i <= arraySize; i++) {
     const row: any = {
-      no: i + 1,
+      no: i,
+      email: `foo${i}@bar.com`,
+      name: `skkim-${i}`,
+      totalStatus: 'complete',
       deleted: false
     };
     
       for (let j = 1; j <= columnSize; j++) {
         row[`status::${j}`] = {
-          label: `data ${i + 1} - ${j}`
+          label: `data ${i} - ${j}`
         };
       }
       
@@ -17,4 +20,49 @@ export const getVirtualColumnTableExampleData = (arraySize: number, columnSize: 
   }
   
   return data;
+};
+
+export const getVirtualColumnTableExampleHeaders = (size: number) => {
+  const exampleFirstHeaders: any[] = [
+    {
+      id: 1,
+      rowspan: 2,
+      label: 'no',
+      width: 65
+    },
+    {
+      id: 2,
+      rowspan: 2,
+      label: 'email',
+      width: 300,
+    },
+    {
+      id: 3,
+      label: 'name',
+      width: 80
+    },
+    {
+      id: 4,
+      colspan: size,
+      label: 'statuses'
+    }
+  ];
+  const exampleSecondHeaders: any[] = [
+    {
+      id: 1,
+      label: 'totalStatus',
+    }
+  ];
+  
+  for (let i = 1; i <= size; i++) {
+    exampleSecondHeaders.push({
+      id: i + 1,
+      label: `${i}`,
+    });
+  }
+  
+  return {
+    exampleFirstHeaders,
+    exampleSecondHeaders
+  };
 };
